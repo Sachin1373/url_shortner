ef5247ac2a1a7654899a195edead2456b716dc10
Sachin Rathod <sachinvrathod1373@gmail.com>
2025-01-29 02:12:56 +0530

    cors update

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 databaseconnect()
 
 const corsOptions = {
-    origin: ['https://url-shortner-three-sigma.vercel.app','http://localhost:5173'],
+    origin: ['https://url-shortner-three-sigma.vercel.app','http://localhost:5174'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],          
     credentials: true,                                 
   };
