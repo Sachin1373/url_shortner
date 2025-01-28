dbda9e37ffb080460c14c31e8bea5895cf09d112
Sachin Rathod <sachinvrathod1373@gmail.com>
2025-01-28 05:50:56 +0530

    redirect logic check

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import SignUp from './pages/SignUp'
+import Redirect from './Components/redirect';
 import Login from './pages/Login'
 import Dashboard from './pages/dashboard';
 import './App.css'
@@ -14,6 +15,7 @@ function App() {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="/redirect/:id" element={<Redirect />} />
       </Routes>
     </Router>
   )
